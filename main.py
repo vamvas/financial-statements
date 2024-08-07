9b840afc395da3dde38a88b7c53ddae178e4ea00
alex vamv <vamvas@outlook.com.gr>
2024-08-07 13:52:45 +0100

    Refactoring main script

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.data_loader.file_loader import load_csv
 from src.transformer.google_sheet_entry import GoogleSheetEntry
 from src.google_sheets.google_sheets_api import GoogleSheet
 
+
 # Load Data from csv
 transactional_data = load_csv("raw_data/transactional_data.csv")
 
@@ -12,5 +13,6 @@ GoogleSheet(
     "credentials/google_service_account.json",
     "1GI3MmJm-vJTPEm1fC5C03JD3i2Riw5d9GWCwiQLsMp4"
     ).write_data_to_google_sheet_tab(
-        "Data", google_sheet_data
+        "Data",
+        google_sheet_data
         )
