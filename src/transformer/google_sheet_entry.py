46801a3f79ec8bdd99339c4b9eb23006ab1eb325
alex vamv <vamvas@outlook.com.gr>
2024-08-07 01:09:14 +0100

    Adding transformer that creates input for google sheet api

---
+++ src/transformer/google_sheet_entry.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+class GoogleSheetEntry():
+
+    def __init__(self, list_of_dict: list[dict[str, str]]):
+        self.list_of_dict = list_of_dict
+        self.df = self.__create_df_from_list_of_dict()
+
+    def __create_df_from_list_of_dict(self) -> pd.DataFrame:
+
+        transactions_df = pd.DataFrame(
+            data=self.list_of_dict,
+            columns=[keys for keys in self.list_of_dict[0]],
+            index=range(len(self.list_of_dict))
+            ).astype(
+                {
+                    "Date": "datetime64[ns]",
+                    "Value": "float64"
+                }
+            )
+
+        transactions_df = transactions_df.loc[
+            :, ["Date", "Description", "Value"]
+            ]
+
+        return transactions_df
